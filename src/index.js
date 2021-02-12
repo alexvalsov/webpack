b12167f74ce6d04e6d521135441a956469220950
Alex Vlasov <alexvlasov@Alexs-MacBook-Pro.local>
2021-02-12 13:21:13 +0200

    start template

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// JS
+import './js/'
+
+// Bootstrap 4
+import 'bootstrap';
+
+import 'jquery';
+
+import popper from 'popper.js';
+
+// SCSS
+import './assets/scss/main.scss'
+
+
+// CSS (example)
+// import './assets/css/main.css'
+
+
